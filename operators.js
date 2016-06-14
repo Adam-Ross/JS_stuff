c88f02bfc54975ab0937fc6df765374744c87f33
Adam Ross <adamgarrettross@gmail.com>
2016-06-14 14:36:07 -0500

    Tur operator.

---
+++ operators.js
@@ -0,0 +1,12 @@
+var x = 5
+// var isEven;
+//
+// if (x % 2 === 0)
+//   isEven = true;
+// else
+//   isEven = false;
+//
+//   alert(isEven);
+
+var isEven = (x % 2 === 0) ? true : false;
+alert(isEven);
